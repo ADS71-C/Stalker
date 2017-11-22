7ad7a5ea9a01f9a8e057f75fc7116b9133705df3
Mike Evers <mike_evers1997@hotmail.com>
2017-11-22 13:38:38 +0100

    delete soft-boiled, removed to other repo

--- soft-boiled/doc/README.md
+++
@@ -1,9 +0,0 @@
-# Generating Documentation
-
-We use Sphink document builder
-
-### Build Requirements
-- pip install -U Sphinx
-
-### To build the documentation
-- make html
--- soft-boiled/src/utils/schema.py
+++
@@ -1,8 +0,0 @@
-""" Get the schema we have inferred so that schema is constant"""
-from pyspark.sql.types import StructType
-import json
-
-def get_twitter_schema(json_file_name):
-    schema_dict = json.load(open(json_file_name))
-    schema_struct = StructType.fromJson(schema_dict)
-    return schema_struct
